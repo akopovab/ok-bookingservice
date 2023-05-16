76f9772076c06aa5e5b70db2cbbca956c3b045e8
akopov_ab <akopov_ab@ursip.ru>
2023-05-16 13:37:11 +0300

    api-spec version1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,14 @@ rootProject.name = "ok-bookingservice"
 
 pluginManagement {
     val kotlinVersion: String by settings
+    val openapiVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
+        id("org.openapi.generator") version openapiVersion apply false
     }
 }
 
-include("m1l1-quickstart")
-include("ok-bookingservice-test")
+//include("m1l1-quickstart")
+//include("ok-bookingservice-test")
+include("ok-bookingservice-api")
